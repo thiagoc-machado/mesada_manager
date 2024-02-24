31a7eb44d1272c9cc78234e9e2a74e0312fe12da
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-02-24 18:46:06 +0100

    gunicorn

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install -r requirements.txt
 
 COPY . /app/
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["gunicorn", "mesada_manager.wsgi:application", "--bind", "0.0.0.0:8000"]
--- mesada_manager/settings.py
+++ mesada_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=i7qwr8goks1=9v(z8=gm^b^a7yld9b+oesdx#^bd9vpo#+@9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mesada.up.railway.app']
 
 
 # Application definition
