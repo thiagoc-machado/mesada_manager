3d1ff6b5839dfade1c374fe286b09f457a4f8000
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-03-05 10:23:09 +0100

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
+python manage.py collectstatic --no-input --settings=mesada-manager.settings
 
 echo 'Applying migrations...'
-python manage.py wait_for_db --settings=config.settings.production
-python manage.py migrate --settings=config.settings.production
+python manage.py wait_for_db --settings=mesada-manager.settings
+python manage.py migrate --settings=mesada-manager.settings
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn --env DJANGO_SETTINGS_MODULE=mesada-manager.settings mesada-manager.wsgi:application --bind 0.0.0.0:$PORT
